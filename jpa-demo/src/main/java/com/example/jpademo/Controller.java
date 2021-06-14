515716e37ceaeb516bf84b97ed9e35fc4f68fbc6
Diego Ezquerro Calvo <diego.ezquerro@bosonit.com>
2021-06-14 12:18:03 +0200

    select all users

--- jpa-demo/src/main/java/com/example/jpademo/Controller.java
+++ jpa-demo/src/main/java/com/example/jpademo/Controller.java
@@ -7,6 +7,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 public class Controller {
 
@@ -14,8 +16,8 @@ public class Controller {
     RepositoryUser repositoryUser;
 
     @GetMapping
-    public String getHola(){
-        return "Hello";
+    public List<User> getUsers(){
+        return repositoryUser.findAll();
     }
 
     @PostMapping
